aa8e8f6c13d9b72217fa9c32b557f6696c589326
minjaeKim <ruy0701@gmail.com>
2022-03-16 09:58:29 +0900

    Initial commit

---
+++ src/main/java/com/spring/spring_0316/Spring0316Application.java
@@ -0,0 +1,13 @@
+package com.spring.spring_0316;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring0316Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring0316Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/spring_0316/Spring0316ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.spring_0316;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring0316ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
